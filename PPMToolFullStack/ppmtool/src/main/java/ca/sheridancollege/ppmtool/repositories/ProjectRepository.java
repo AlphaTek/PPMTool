b59dae7c92494bdf181c456e19e3233e8b283f5d
awasif647 <awasif647@gmail.com>
2019-09-15 00:23:09 -0400

    branch6 Refactoring

---
+++ PPMToolFullStack/ppmtool/src/main/java/ca/sheridancollege/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,19 @@
+package ca.sheridancollege.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import ca.sheridancollege.ppmtool.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long>{
+
+	@Override
+	default Iterable<Project> findAllById(Iterable<Long> ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
