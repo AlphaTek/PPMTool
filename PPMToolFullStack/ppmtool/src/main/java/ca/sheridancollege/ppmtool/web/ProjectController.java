97b4bce80dac3207f3a61f7f09ae306be0414c47
awasif647 <awasif647@gmail.com>
2019-09-15 00:36:10 -0400

    Refactored

--- PPMToolFullStack/ppmtool/src/main/java/ca/sheridancollege/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/ppmtool/src/main/java/ca/sheridancollege/ppmtool/web/ProjectController.java
@@ -17,35 +17,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.sheridancollege.ppmtool.domain.Project;
+import ca.sheridancollege.ppmtool.services.MapValidationErrorService;
 import ca.sheridancollege.ppmtool.services.ProjectService;
 
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
-	
+
 	@Autowired
 	private ProjectService projectService;
 	
+	@Autowired
+	private MapValidationErrorService mapValidationErrorService;
 	@PostMapping("")
-	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {	
+	public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
+
+		ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+		if(errorMap!=null) return errorMap;
 		
-		if(result.hasErrors()) {
-			
-			Map<String, String> errorMap = new HashMap<>();
-			
-			for(FieldError error: result.getFieldErrors()) {
-				errorMap.put(error.getField(), error.getDefaultMessage());
-				
-			}
-			
-			return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-		}
-		
-	
-		
+
 		Project project1 = projectService.saveOrUpdateProject(project);
 		return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
 	}
-	
 
 }
